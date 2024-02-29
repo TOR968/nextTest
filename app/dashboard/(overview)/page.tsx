89d5e5dfd716b58a844b189bafcfa9e3f95870b8
Maxim Turbal <turbalmaxim@gmail.com>
2024-02-29 21:20:38 +0200

    add pagination

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,12 +1,13 @@
 import { Card } from '@/app/ui/dashboard/cards';
 import IncomeChart from '@/app/ui/dashboard/income-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
+import CardWrapper from '@/app/ui/dashboard/cards';
 import { poppins } from '@/app/ui/fonts';
-import { fetchIncome, fetchLatestInvoices, fetchCardData} from '@/app/lib/data';
+import { fetchCardData} from '@/app/lib/data';
+import { Suspense } from 'react';
+import { IncomeChartSkeleton, LatestInvoicesSkeleton, CardsSkeleton } from '@/app/ui/skeletons';
 
 export default async function Page() {
-    const income = await fetchIncome();
-    const latestInvoices = await fetchLatestInvoices();
     const {
         numberOfSellers,
         numberOfInvoices,
@@ -22,18 +23,17 @@ export default async function Page() {
                 Dashboard
             </h1>
             <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-                <Card title="Earned" value={totalFulfilledInvoices} type="earned" />
-                <Card
-                title="In Progress"
-                value={totalAwaitingInvoices}
-                type="awaiting"
-                />
-                <Card title="All Invoices" value={numberOfInvoices} type="invoices" />
-                <Card title="Total Sellers" value={numberOfSellers} type="sellers" />
+                <Suspense fallback={<CardsSkeleton />}>
+                    <CardWrapper />
+                </Suspense>
             </div>
             <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-                <IncomeChart income={income} />
-                <LatestInvoices latestInvoices={latestInvoices} />
+                <Suspense fallback={<IncomeChartSkeleton />}>
+                    <IncomeChart />
+                </Suspense>
+                <Suspense fallback={<LatestInvoicesSkeleton />}>
+                    <LatestInvoices />
+                </Suspense>
             </div>
         </main>
     );
